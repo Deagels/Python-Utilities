4d8b4e7d0349048a1d0111980fa2780037b5df5f
Deagels <joakim.hagen@lyse.net>
2015-12-15 22:18:05 +0100

    convert indentation to 4 spaces

--- pimport.py
+++ pimport.py
@@ -20,22 +20,22 @@ from importlib import _bootstrap # evil hacks
 
 inner_fal = _bootstrap._find_and_load
 def _find_and_load(name, import_):
-	pimpname = "pimp$"+ name
-	module = sys.modules[pimpname] if pimpname in sys.modules \
-		else inner_fal(name, import_)
-	caller = caller_module()
-	mod    = module
-	if "__new__" in mod.__dict__:
-		module = mod.__new__(mod, caller)
-	if "__init__" in mod.__dict__:
-		mod.__init__(mod, caller)
-	# remove cache in order to force us back here again next import
-	del sys.modules[name] 
-	return module
+    pimpname = "pimp$"+ name
+    module = sys.modules[pimpname] if pimpname in sys.modules \
+        else inner_fal(name, import_)
+    caller = caller_module()
+    mod    = module
+    if "__new__" in mod.__dict__:
+        module = mod.__new__(mod, caller)
+    if "__init__" in mod.__dict__:
+        mod.__init__(mod, caller)
+    # remove cache in order to force us back here again next import
+    del sys.modules[name] 
+    return module
 _bootstrap._find_and_load = _find_and_load
 
 def caller_module():
-	try: name = inspect.currentframe().f_back.f_back.f_globals["__name__"]
-	except KeyError: raise ValueError("__name__ is not in the global namespace")
-	try: return sys.modules[name]
-	except KeyError: raise ValueError("Cannot find module named '%s' in sys.modules. Have __name__ been modified?" % name)
+    try: name = inspect.currentframe().f_back.f_back.f_globals["__name__"]
+    except KeyError: raise ValueError("__name__ is not in the global namespace")
+    try: return sys.modules[name]
+    except KeyError: raise ValueError("Cannot find module named '%s' in sys.modules. Have __name__ been modified?" % name)
